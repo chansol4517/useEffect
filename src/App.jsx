ec3721d4bd96477bf6b4dc48ae962eb62d90fe66
chansol4517 <ckssol4517@gmail.com>
2024-10-03 23:30:54 +0900

    컴포넌트 리랜더시의 생명주시 활용 사례

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,27 @@
 import { useState, useEffect } from 'react';
+import Modal from './Modal';
 
 function App() {
 	console.log('App Render');
-	const [Members, setMembers] = useState([]);
 
-	//아래와 같이 useEffect를 처리하지 않은 상태에서 서버에서 fetching한 데이터를 state에 담으면 무한로딩 발생
-	// fetch("/member.json")
-	// 	.then(data => data.json())
-	// 	.then(json => {
-	// 		console.log(json.members);
-	// 		setMembers(json.members);
-	// 	});
+	let [IsOpen, setIsOpen] = useState(false);
 
+	//의존성 배열에 IsOpen state를 등록해서
+	//모달창 출력 유무의 정보값이 변경될때에만 전용함수인 body요소의 스크롤바 제거, 생성의 기능을 선별적으로 호출
 	useEffect(() => {
-		//의존성 배열이 비어있는 useEffect문의 콜백함수는 아무리 해당 컴포넌트가 재랜더링되더라도
-		//처음 마운트시 한번만 호출되므로 fetch함수가 처음 한번만 데이터를 가져오고 더이상 무한호출에 빠지지 않음
-		//실무에서 React-Query를 쓰기 전까지는 제일 많이 쓰이는 리액트 개발 패턴 (중요)
-		fetch('/member.json')
-			.then(data => data.json())
-			.then(json => {
-				setMembers(json.members);
-			});
-	}, []);
+		if (IsOpen) {
+			document.body.style.overflow = 'hidden';
+		} else {
+			document.body.style.overflow = 'auto';
+		}
+	}, [IsOpen]);
 
 	return (
-		<>
+		<main className='h-[300vh] w-full'>
 			<h1>useEffect</h1>
-			{Members.map((data, idx) => (
-				<h2 key={idx}>{data.name}</h2>
-			))}
-		</>
+			<button onClick={() => setIsOpen(!IsOpen)}>모달창 토글</button>
+			{IsOpen && <Modal />}
+		</main>
 	);
 }
 
---
+++ src/Modal.jsx
@@ -0,0 +1,5 @@
+function Modal() {
+	return <aside className='fixed left-20 top-20 h-[300px] w-[500px] bg-blue-400'>Modal</aside>;
+}
+
+export default Modal;
